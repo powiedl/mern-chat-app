f299ab43b22192c4a8d823b60fc8dd04b202db19
powiedl <rolandpi@gmx.at>
2025-06-27 06:00:56 +0200

    integrate frontend into the backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import { app, server } from './socket/socket.js';
 
@@ -13,12 +14,20 @@ dotenv.config();
 
 const PORT = process.env.PORT || '5000';
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser()); // to parse the incoming cookies from req.cookies
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+
+// serve the frontend from our backend https server
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend', 'dist', 'index.html'));
+});
 
 // app.get('/', (req, res) => {
 //   // root route
